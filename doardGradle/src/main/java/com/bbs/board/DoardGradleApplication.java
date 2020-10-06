6f08c81f41736152013d24729fce89860292c172
B202 <B202@B202-03>
2020-10-06 11:26:51 +0900

    최초업로드

---
+++ doardGradle/src/main/java/com/bbs/board/DoardGradleApplication.java
@@ -0,0 +1,13 @@
+package com.bbs.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoardGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoardGradleApplication.class, args);
+	}
+
+}
